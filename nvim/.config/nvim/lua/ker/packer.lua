063679c79a8d5a7961ae226d4b6ef9db3fc6f468
Kertonal <>
2023-05-01 12:08:25 -0400

    Disable neoscroll in nvim

--- nvim/.config/nvim/lua/ker/packer.lua
+++ nvim/.config/nvim/lua/ker/packer.lua
@@ -76,11 +76,13 @@ return require('packer').startup(function(use)
         config = function() require("nvim-autopairs").setup {} end
     };
 
+    --[[
     use{
         "karb94/neoscroll.nvim",
         config = function()
             require("neoscroll").setup()
         end
     };
+    --]]
 }
 end)
