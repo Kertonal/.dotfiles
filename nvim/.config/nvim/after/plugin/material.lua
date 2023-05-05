ebddd36e6418563357353e924c298b55b6e9487c
Kertonal <>
2023-05-05 09:58:06 -0400

    Fix nvim gitsigns color in Material colorscheme

--- nvim/.config/nvim/after/plugin/material.lua
+++ nvim/.config/nvim/after/plugin/material.lua
@@ -3,7 +3,11 @@ require('material').setup({
         'gitsigns',
         'nvim-cmp',
         'telescope'
-    }
+    },
+    custom_colors = function(colors)
+        colors.git.added    = '#27e002'
+        colors.git.modified = '#f8fc0d'
+    end
 })
 
 vim.cmd('colorscheme material')
