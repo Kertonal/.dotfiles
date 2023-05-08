5477f7d747f8e94edde32bfb98b316cbe25b8e2d
Kertonal <>
2023-05-07 23:23:50 -0400

    Add nvim remap to use tmux-sessionizer script
    Added <C-f> for now to execute this script. Assumes already in a
    tmux session.

--- nvim/.config/nvim/lua/ker/remap.lua
+++ nvim/.config/nvim/lua/ker/remap.lua
@@ -20,3 +20,5 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
