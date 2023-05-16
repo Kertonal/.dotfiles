25d0e99928c1d91bda69196b58bdbc2625890bed
Kertonal <>
2023-05-16 15:00:26 -0400

    Setup nvim remaps for special yank register
    Setup remaps to yank, delete, and paste with a specific register
    using the leader key. This is used to copy or cut text to and yank
    later without it being overwritten by the annoying default yank
    register.

--- nvim/.config/nvim/lua/ker/remap.lua
+++ nvim/.config/nvim/lua/ker/remap.lua
@@ -12,12 +12,17 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-j>", "3<C-e>")
 vim.keymap.set("n", "<C-k>", "3<C-y>")
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
+-- yanking to the clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- yanking, deleting, and pasting with a {r}emembered register
+vim.keymap.set({"n", "v"}, "<leader>ry", [["ry]])
+vim.keymap.set({"n", "v"}, "<leader>rd", [["rd]])
+vim.keymap.set("n", "<leader>rY", [["rY]])
+vim.keymap.set("n", "<leader>rD", [["rD]])
+vim.keymap.set({"n", "v"}, "<leader>rp", [["rp]])
+vim.keymap.set({"n", "v"}, "<leader>rP", [["rP]])
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
